37012a1ada08905f430d5e3de7790cce0b7e9cca
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-02-02 23:14:01 -0800

    added some animation to the fab icon for the speed dial

--- lib/components/fab_menu.dart
+++ lib/components/fab_menu.dart
@@ -14,14 +14,39 @@ class FloatingActionButtonMenuState extends State<FloatingActionButtonMenu>
     with SingleTickerProviderStateMixin {
   final List<FloatingActionButton> _actions;
 
+  AnimationController _animationController;
+  Animation<double> _iconAnimation;
+
   FloatingActionButtonMenuState(actions) : _actions = actions;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200))
+          ..addListener(() {
+            setState(() {});
+          });
+
+    _iconAnimation =
+        new Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new FloatingActionButton(
       onPressed: () {},
       tooltip: 'Increment',
-      child: Icon(Icons.add),
+      child: AnimatedIcon(
+        icon: AnimatedIcons.menu_close,
+        progress: _iconAnimation,
+      ),
     );
   }
 }
